276a1cea53e625185f6872de07aedc636a6f19db
gsousaaa <glaucoezequiel94@gmail.com>
2024-02-27 16:09:38 -0300

    Estabelecendo conexão ao socketIO

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,24 @@
 import express from "express";
 import path from "path";
 import dotenv from 'dotenv';
+import { createServer } from 'http'
+import { Server } from 'socket.io'
 
 dotenv.config()
 
-const server = express()
+const app = express()
+const server = createServer(app)
+const io = new Server(server)
 
-server.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, '../public')))
 
 server.listen(process.env.PORT, ()=> {
     console.log(`server rodando na porta ${process.env.PORT}`)
 })
+
+io.on('connection', (socket)=> {
+    console.log('Conexão detectada')
+})
+
 
 
