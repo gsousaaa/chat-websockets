ec312642deded4a3d83a9858ad96f68b74f9c0f6
gsousaaa <glaucoezequiel94@gmail.com>
2024-02-27 11:52:17 -0300

    configuração do servidor

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import path from "path";
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+const server = express()
+
+server.use(express.static(path.join(__dirname, 'public')))
+
+server.listen(process.env.PORT, ()=> {
+    console.log(`server rodando na porta ${process.env.PORT}`)
+})
