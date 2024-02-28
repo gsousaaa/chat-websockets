5c30b5922e0702d949fd146f62854c8d697ab3e2
gsousaaa <glaucoezequiel94@gmail.com>
2024-02-28 13:05:12 -0300

    Criação do listener send-msg, para o envio das mensagens, e do emit show msg, para mostrar as mensagens

--- src/server.ts
+++ src/server.ts
@@ -55,6 +55,15 @@ io.on('connection', (socket) => {
             list: connectedUsers
         } )
     })
+
+    socket.on('send-msg', (txt) => {
+        let obj = {
+            username: socket.username,
+            message: txt
+        }
+        
+        socket.broadcast.emit('show-msg', obj)
+    })
 })
 
 
