82fe09a78278ec3aa9385a563e95cd582afc932a
gsousaaa <glaucoezequiel94@gmail.com>
2024-02-28 12:34:00 -0300

    Criação da função addMessage, para quando os usuários forem conectados e desconectados do chat.

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,7 +1,7 @@
 const socket = io();
 const chatPage = document.getElementById('chat')
 const chatArea = document.querySelector('.chatArea')
-const chatList = document.querySelector('.chatList')
+
 
 let userList = []
 
@@ -16,6 +16,20 @@ function renderUserList(users) {
     console.log(userList)
 }
 
+function addMessage(type, user, msg) {
+    let ul = document.querySelector('.chatList')
+   
+    switch(type) {
+        case 'status':
+            ul.innerHTML += `<li class="m-status">${msg}</li>`
+        break; 
+        case 'msg': 
+             ul.innerHTML += `<li class="m-status"><span>${user}</span> ${msg}</li>`
+        break;
+    }
+
+}
+
 window.addEventListener('load', () => {
     const username = sessionStorage.getItem('username');
     if (username) {
@@ -26,11 +40,21 @@ window.addEventListener('load', () => {
 
 
 socket.on('user-ok', (list) => {
+    addMessage('status', null, 'Conectado!')
+
     userList = list
     renderUserList(userList)
 })
 
 socket.on('list-update', (data) => {
+    if (data.joined) {
+        addMessage('status', null, `${data.joined} entrou no chat`)
+    }
+
+    if (data.left) {
+        addMessage('status', null, `${data.left} saiu no chat`)
+    }
+
     userList = data.list
     renderUserList(userList)
 
